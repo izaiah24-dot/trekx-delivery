374ab1fd765c34a77dca849691be5b4ca0d6ac6e
izaiah24-dot <gabrieleffiong574@gmail.com>
2026-07-31 04:17:39 +0100

    Update main.jsx

--- TrekX-Delivery-Web-App/src/main.jsx
+++ TrekX-Delivery-Web-App/src/main.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from "react";
 import {createRoot} from "react-dom/client";
+import logo from "./public_logo.jpg";
 import {createClient} from "@supabase/supabase-js";
 import {Bike,MapPin,Package,User,Phone,LogIn,Menu,X,CheckCircle} from "lucide-react";
 import "./style.css";
